990debf6db985e6589014fc9100e478e859ad259
Jvaljer <abbel.haol@gmail.com>
2023-11-21 11:27:38 +0100

    finally stopped being dumb and implemented login + first views with navigation (no data check yet)

--- App/app.js
+++ App/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use(express.static(__dirname));
 // Adding css and js files from installed apps
@@ -22,16 +23,13 @@ app.set('layout', './layouts/app-layout.ejs')
 app.get('', (req, res) => {
     res.render("login-view.ejs")
 });
-app.get('/logged', (req, res) => {
-    res.render("fst-view.ejs")
-});
 
 //handling logins check
 app.post('/login', (req, res) => {
-    const is_valid = true;
-    if(is_valid){
-        res.render("fst-view.js");
-    }
+    res.render("fst-view.ejs");
+});
+app.post('/new-trip', (req,res) => {
+    res.render("new-trip-view.ejs");
 });
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
