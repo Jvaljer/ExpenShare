7d2503b90b142dd0fd2b9dae73b946e0a31d3f08
Jvaljer <abbel.haol@gmail.com>
2023-11-27 22:16:17 +0100

    finally fixed chart creation -> gotta move on and follow TODO list

--- App/public/js/travel-main.js
+++ App/public/js/travel-main.js
@@ -1,13 +1,32 @@
 var my_canva = document.getElementById('myChart');
 console.log("fetched my canva");
 
-const chart = new Chart(my_canva, {
-   type: "bar",
-   data: {
-       labels: ["C1", "C2", "C3"],
-       datasets: [{
-           data: [240, 120, 140, 130]
-       }]
-   }
-});
-console.log("created my chart");
+const data = {
+    labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }]
+};
+
+const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        aspectRatio: 1,
+    },
+};
+
+const chart = new Chart(my_canva, config);
